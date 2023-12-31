5246d224dd2478df46fc33b500a3cb62e2da0cb9
DotNetTechies@outlook.com <DotNetTechies@outlook.com>
2024-01-01 00:19:21 +0530

    Added Category Module and created N-tier Architechture.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,94 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext applicationDbContext) { 
+            _db = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _db.Categories.ToList();
+            return View(categories);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                TempData["success"]= "Category added successfully";
+                return RedirectToAction("Index", "category");
+            }
+            return View();
+        }
+
+        public IActionResult Edit( int? Id)
+        {
+            if (Id == 0 || Id == null)
+            {
+                return NotFound();
+            }
+            Category category = _db.Categories.FirstOrDefault(c => c.Id == Id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index", "category");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == 0 || Id == null)
+            {
+                return NotFound();
+            }
+            Category category = _db.Categories.FirstOrDefault(c => c.Id == Id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? Id)
+        {
+            Category category = _db.Categories.FirstOrDefault(c => c.Id == Id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index", "category");
+            
+        }
+    }
+}
